03d7957abea0704e4fb1cf8e0519c53d211db49b
Avi-GitH522 <abhishek.maharana522@gmail.com>
2026-03-09 10:12:25 +0530

    Create SQL query for patients with names starting and ending with 's'
    Add SQL query to retrieve patients with specific name criteria.

---
+++ sql-practice/question_3.sql
@@ -0,0 +1,8 @@
+-- Question: Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+SELECT
+	patient_id,
+  first_name
+FROM patients
+WHERE 
+	first_name Like 's%s' AND
+  LEN(first_name) >= 6
