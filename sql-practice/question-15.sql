8b7d41b889f61a9f5481b16cc13033badef83ece
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-03-13 23:37:09 +0530

    Delete sql-practice/question-15.sql

--- sql-practice/question-15.sql
+++
@@ -1,6 +0,0 @@
--- Question: Show the difference between the largest weight and smallest weight for patients with the last name 'Maroni'
-SELECT
-	MAX(weight) - MIN(weight) as weight_delta
-FROM patients
-WHERE
-	last_name = 'Maroni'
