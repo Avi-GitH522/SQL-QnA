293129f3849ed75127e6e3d9df00b22b890c322d
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-03-20 21:11:26 +0530

    Add SQL query to retrieve occupation data

--- HackerRank/The_PADS.sql
+++ HackerRank/The_PADS.sql
@@ -1,1 +1,29 @@
+SELECT result
+FROM (
+    
+    SELECT 
+        CONCAT(Name, '(', LEFT(Occupation, 1), ')') AS result,
+        1 AS order_col,
+        Name AS sort_col
+    FROM OCCUPATIONS
 
+    UNION ALL
+
+    SELECT 
+        CONCAT(
+            'There are a total of ',
+            COUNT(*),
+            ' ',
+            LOWER(Occupation),
+            's.'
+        ) AS result,
+        2 AS order_col,
+        COUNT(*) AS sort_col
+    FROM OCCUPATIONS
+    GROUP BY Occupation
+
+) t
+
+ORDER BY 
+    order_col,
+    sort_col;
