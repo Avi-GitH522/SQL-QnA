e2df090173bf00555bbd02e3c7e1834f6234e422
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-03-25 08:44:22 +0530

    Add SQL query to merge customers and suppliers

---
+++ sql-practice/question_northwind_3.sql
@@ -0,0 +1,14 @@
+-- Question: Show the city, company_name, contact_name from the customers and suppliers table merged together.
+--Create a column which contains 'customers' or 'suppliers' depending on the table it came from.
+
+SELECT
+	city, company_name, contact_name,
+    'customers' as role
+FROM customers
+
+union
+
+SELECT
+	city, company_name, contact_name,
+    'suppliers' as role
+FROM suppliers
