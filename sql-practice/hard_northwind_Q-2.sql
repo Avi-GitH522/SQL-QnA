38ae0a527a9925b0158308d2e27fa786288fdcf2
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-03-27 21:21:22 +0530

    Implement query for yearly discount loss calculation
    Add a query to calculate annual discount losses.

--- sql-practice/hard_northwind_Q-2.sql
+++ sql-practice/hard_northwind_Q-2.sql
@@ -1,1 +1,13 @@
-
+-- Question: Show how much money the company lost due to giving discounts each year, order the years from most recent to least recent. Round to 2 decimal places
+SELECT
+	year(o.order_date) as order_year,
+    ROUND(
+        sum(p.unit_price*od.quantity*od.discount)
+      ,2) as discount_amount
+FROM products p
+JOIN order_details od
+	ON p.product_id = od.product_id
+JOIN orders	o 
+    on od.order_id = o.order_id
+GROUP BY YEAR(o.order_date)
+ORDER BY YEAR(o.order_date) DESC
