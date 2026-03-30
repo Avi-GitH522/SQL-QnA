fa62c819e4c6c7e5ba75107342390ab5f2a77400
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-03-30 09:49:26 +0530

    Delete DataLemur/Page_With_No_Likes.sql

--- DataLemur/Page_With_No_Likes.sql
+++
@@ -1,9 +0,0 @@
--- Question: Write a query to return the IDs of the Facebook pages that have zero likes. The output should be sorted in ascending order based on the page IDs.
-SELECT
-  p.page_id
-FROM pages p 
-LEFT JOIN page_likes pl 
-ON 
-  pl.page_id = p.page_id
-WHERE pl.liked_date IS NULL
-ORDER BY p.page_id;
