bfbbf013a8d23ee0bb76693bfb5379f346216717
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-03-31 08:59:32 +0530

    Implement query for user post date differences in 2021
    Add SQL query to calculate days between first and last posts for users in 2021.

---
+++ DataLemur/Easy/Average_Post_Hiatus(Part-1).sql
@@ -0,0 +1,11 @@
+-- Question: Given a table of Facebook posts, for each user who posted at least twice in 2021, 
+-- write a query to find the number of days between each user’s first post of the year and last post of the year in the year 2021. 
+-- Output the user and number of the days between each user's first and last post.
+
+SELECT
+  user_id,
+  DATEDIFF(MAX(post_date),MIN(post_date)) as day_diff
+FROM posts 
+WHERE YEAR(post_date) = 2021
+GROUP BY user_id
+HAVING COUNT(post_date) > 1
