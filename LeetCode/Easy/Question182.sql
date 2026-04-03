5f92e7aafc4ddbc06564b9ce3edc36d7446ec9f3
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-04-03 08:52:46 +0530

    Create SQL query for reporting duplicate emails
    Add SQL query to find duplicate emails in the Person table.

---
+++ LeetCode/Easy/Question182.sql
@@ -0,0 +1,6 @@
+-- Question: Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL. Return the result table in any order
+SELECT
+    email
+FROM Person
+GROUP BY email
+HAVING COUNT(*) >= 2
