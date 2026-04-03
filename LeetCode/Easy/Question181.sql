fca656d6d26a4552e41ee15acc532dcb648d6bf5
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-04-03 08:05:30 +0530

    Create SQL query for employee salary comparison
    Add SQL query to find employees earning more than their managers.

---
+++ LeetCode/Easy/Question181.sql
@@ -0,0 +1,10 @@
+-- Question: Write a solution to find the employees who earn more than their managers. Return the result table in any order. The result format is in the following example.
+
+SELECT
+    e.name as Employee
+FROM Employee e
+JOIN Employee m
+ON
+    e.managerId = m.id
+WHERE
+    e.salary > m.salary
