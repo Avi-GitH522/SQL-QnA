ab993ef18ac1881480e9da817f212d188b19b9ab
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-04-05 20:51:29 +0530

    Add solution-2

--- DataLemur/Easy/Histogram_of_Tweets.sql
+++ DataLemur/Easy/Histogram_of_Tweets.sql
@@ -16,3 +16,19 @@ GROUP BY tweet_count
 
 -- Explanation: I need to find out total number of tweets per user as tweet_bucket. Then I'll group again those count numbers to find out number of users fall into that bucket, 
 -- that means number of users who have posted the same number of tweets. (Try to read the quesiton agian and again!)
+
+
+-- Solution-2: 
+
+WITH cte AS (
+SELECT
+  count(*) as tweet_bucket
+FROM tweets
+WHERE 
+  YEAR(tweet_date) = 2022
+GROUP BY user_id
+)
+SELECT
+  tweet_bucket, COUNT(*) as user_num
+FROM cte
+GROUP BY tweet_bucket
