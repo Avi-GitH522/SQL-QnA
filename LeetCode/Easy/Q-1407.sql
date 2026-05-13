716cca1281f6626391b3be95b1a1c56e2fb706bd
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-05-13 10:01:08 +0530

    Add alternative SQL solution for travelled distance
    Added an alternative SQL solution using COALESCE for handling NULL values.

--- LeetCode/Easy/Q-1407.sql
+++ LeetCode/Easy/Q-1407.sql
@@ -15,3 +15,21 @@ Solution-1:
         u.name
     ORDER BY 
         travelled_distance DESC, name
+
+Solution-2:
+
+SELECT 
+    u.name,
+    COALESCE(SUM(r.distance), 0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r
+ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name ASC;
+
+-- As SUM() returns NULL incase of NULL values, COLEESCE() will return 0.
+
+
+
+
+
