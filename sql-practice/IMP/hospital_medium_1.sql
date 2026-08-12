53612b7fc9f412fb8edaa8379abbab91ad6e8251
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-08-12 09:37:20 +0530

    Fix condition for attending doctor ID selection

--- sql-practice/IMP/hospital_medium_1.sql
+++ sql-practice/IMP/hospital_medium_1.sql
@@ -6,6 +6,6 @@ SELECT
 	patient_id, attending_doctor_id, diagnosis
 FROM admissions
 WHERE
-	(patient_id%2 = 1 AND attending_doctor_id IN (1,5,19))
+	(patient_id%2 != 0 AND attending_doctor_id IN (1,5,19))
     OR
     (attending_doctor_id LIKE '%2%' AND LEN(patient_id) = 3)
