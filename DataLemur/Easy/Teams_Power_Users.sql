af0eb1ef23599be557ce3ebca50f7f33298c1958
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-09-03 10:09:50 +0530

    Add SQL query to count messages by sender for August 2022

---
+++ DataLemur/Easy/Teams_Power_Users.sql
@@ -0,0 +1,11 @@
+SELECT
+  sender_id,
+  COUNT(*) AS message_count
+FROM messages
+WHERE
+  EXTRACT(YEAR FROM sent_date) = 2022 AND 
+  EXTRACT(MONTH FROM sent_date) = 8
+GROUP BY 
+  sender_id
+ORDER BY
+  message_count DESC limit 2;
