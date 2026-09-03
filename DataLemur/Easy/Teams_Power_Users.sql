c03b89eaed28cce2287e58a1c86af63b16ecceef
Abhishek Maharana <abhishek.maharana522@gmail.com>
2026-09-03 10:10:31 +0530

    Add comments to Teams_Power_Users.sql for clarity
    Added comments to clarify the purpose of the query for identifying top Power Users on Microsoft Teams.

--- DataLemur/Easy/Teams_Power_Users.sql
+++ DataLemur/Easy/Teams_Power_Users.sql
@@ -1,3 +1,6 @@
+--Write a query to identify the top 2 Power Users who sent the highest number of messages on Microsoft Teams in August 2022. 
+--Display the IDs of these 2 users along with the total number of messages they sent. Output the results in descending order based on the count of the messages.
+
 SELECT
   sender_id,
   COUNT(*) AS message_count
